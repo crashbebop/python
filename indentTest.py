ff419f41bba649223d5349e4f8b32b94aa4bb979
crashbebop <66795589+crashbebop@users.noreply.github.com>
2022-07-04 00:46:44 -0400

    Initial Commit: Adding basic print tests

---
+++ indentTest.py
@@ -0,0 +1,14 @@
+print("This is my indention test")
+
+x = 5
+print("x is:", x)
+
+# syntax 1 : if-else
+if (x > 2):
+    print("Pass")
+else:
+    print("Fail")
+
+# syntax 2 : if-else shorthand (requires python3?)
+# statement_when_True if condition else statement_when_False
+print(True) if (x > 2) else print(False)
